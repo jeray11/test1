e806debccaef88cab6fa0e85a5e0a5707196d0dc
jeray11 <116207661@qq.com>
2016-06-05 12:40:12 +0800

    forth

--- OwinConsole/Program.cs
+++ OwinConsole/Program.cs
@@ -12,7 +12,7 @@ namespace OwinConsole
         {
             using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000"))
             {
-                Console.WriteLine("Press [enter] to quit...123");
+                Console.WriteLine("Press [enter] to quit...123 456");
                 Console.ReadLine();
             }
         }
