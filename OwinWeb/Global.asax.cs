3d20c1d1dd424d6afed01f07cdd5199ae37b988b
jeray11 <116207661@qq.com>
2016-06-05 12:31:54 +0800

    first

---
+++ OwinWeb/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace OwinWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        // How to hook OWIN pipelines into the normal Asp.Net route table side by side with other components.
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Paths that start with /owin will be directed to our startup class.
+            RouteTable.Routes.MapOwinPath("/owin");
+
+            RouteTable.Routes.MapOwinPath("/special", app =>
+            {
+                app.Run(OwinApp2.Invoke);
+            });
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OwinWeb/OwinApp2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OwinWeb
+{
+    public class OwinApp2
+    {
+        // Invoked once per request.
+        public static Task Invoke(IOwinContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello World 2");
+        }
+    }
+}
